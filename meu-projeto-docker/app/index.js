09a63a407fa38a8ed4b732963513ba2bfead27f9
joseph-lins <josephvitor9@gmail.com>
2025-11-21 11:21:26 -0300

    Update index.js

--- meu-projeto-docker/app/index.js
+++ meu-projeto-docker/app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Olá, Joseph! Deploy via Docker Compose funcionando.\n');
+  res.end('Ola, Joseph! Deploy via Docker Compose funcionando.\n');
 });
 
 server.listen(3000);
